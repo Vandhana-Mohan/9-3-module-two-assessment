d17d1f04c11f64ae0c5119dc76f4eab7951033b6
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-01-18 13:33:02 -0500

    add footer

--- main.js
+++ main.js
@@ -1,17 +1,13 @@
 
-
-
-
-
-
 // To ensure Cypress tests work as expeded, add any code/functions that you would like to run on page load inside this function
+// This function will "pause" the functionality expected on load long enough to allow Cypress to fully load
+// So that testing can work as expected for now A non-hacky solution is being researched
 
 function run() {
  // Add code you want to run on page load here
+ const BASE_URL = "https://resource-ghibli-api.onrender.com";
+ 
 }
 
-// This function will "pause" the functionality expected on load long enough to allow Cypress to fully load
-// So that testing can work as expected for now
-// A non-hacky solution is being researched
 
 setTimeout(run, 1000);
