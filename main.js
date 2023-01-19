a02ad2b0a7010a1cfec6cd1e747a9b467bf22fa6
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-01-19 00:10:17 -0500

    pass 19 test

--- main.js
+++ main.js
@@ -11,6 +11,13 @@
  const display__movieHeading = document.querySelector("#movie__heading")
  const display__movieReleaseYear = document.querySelector("#movie__release")           
  const display__movieDescription = document.querySelector("#movie__description")              
+ const submit__review = document.querySelector("#submit")
+ const user__Reviews = document.querySelector("#ulUser__Reviews")
+ const input__Review = document.querySelector("#review")
+ const reset__Review = document.querySelector("#reset-reviews")
+ const show__People = document.querySelector("#show-people")
+ const ol__People = document.querySelector("#ol__people")
+ let selectedMovie;
 
  function run() {
  // Add code you want to run on page load here
@@ -30,19 +37,54 @@ function generateMovieTitles(results){
         option__title.textContent = movie.title
         movieTitles.append(option__title)
     });
-    movieTitles.addEventListener("change", (event) => { updateMovieInfo(results, event) });
+    movieTitles.addEventListener("change", (event) => { 
+        updateMovieInfo(results, event)
+     });
 }
 
 function updateMovieInfo(results, event) {
     event.preventDefault();
-    const selectedId = event.target.value
-    const movie = results.find((movie) => movie.id === selectedId)
-    display__movieHeading.textContent = movie.title;
-    display__movieReleaseYear.textContent = movie.release_date;
-    display__movieDescription.textContent = movie.description;
+    const selectedId = event.target.value;
+    selectedMovie = results.find((movie) => movie.id === selectedId);
+    display__movieHeading.textContent = selectedMovie.title;
+    display__movieReleaseYear.textContent = selectedMovie.release_date;
+    display__movieDescription.textContent = selectedMovie.description;
 }
 
+submit__review.addEventListener("click", (event) => {
+    event.preventDefault();
+    if (!selectedMovie) {
+        alert("Please select a movie first");
+        return
+    }
+    displayUserReviews(selectedMovie);
+});
 
+function displayUserReviews(movie){
+    const list__Reviews = document.createElement("li")
+    list__Reviews.innerHTML = `<strong>${movie.title}:</strong> ${input__Review.value}`;
+    user__Reviews.append(list__Reviews)
+    input__Review.value = ""
+    movieTitles.selectedIndex = 0;
+}
 
+reset__Review.addEventListener("click", (event) => {
+    event.preventDefault()
+    const liElement = user__Reviews.querySelectorAll("li");
+    for (let i = 0; i < liElement.length; i++) {
+        user__Reviews.remove(liElement[i]);
+    }
+});
 
+show__People.addEventListener("click", (event) => {
+    event.preventDefault()
+    fetch (PEOPLE_URL)
+        .then((response)=>response.json())
+        .then((data)=>{
+            console.log(data)
+            const list__people = document.createElement("li")
 
+            list__people.textContent = data.name
+            ol__People.append(list__people)
+    })
+})
